2cb4762c7e7fb2b3c633cab0ed1f4fdac29e1882
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2018-06-07 09:01:19 +0700

    fixed search navigator

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Pictlr - Welcome</title>
+	<title>Opinion - Welcome</title>
 	<meta charset=utf-8>
     <meta name=description content="">
     <meta name=viewport content="width=device-width, initial-scale=1">
